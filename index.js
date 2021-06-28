31e6acb82c1e3259a7603221204aa60441d9e06b
Choi Min-ho <minho85zero@naver.com>
2021-06-28 11:08:11 +0900

    설정파일 추가

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const config = require('./config/key');
+const { User } = require("./model/User");
 
-
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+// application/json
+app.use(bodyParser.json());
 
 // DB 연결
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://zero:1234@study.glolh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false // 에러가 안뜨도록.
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!111')
 })
 
+// 회원가입 
+app.post('/register', (req, res) => {
+  // 회원 가입에 필요한 정보들을 client 에서 받음
 
+  // 그 정보를 데이터 베이스에 넣어준다.(mongodb)
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+
+})
 
 
 
