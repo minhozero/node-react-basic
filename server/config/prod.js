95b1b69b54d2f1987a6483e9b6bcd5cfbb0b846f
minhozero <46142679+minhozero@users.noreply.github.com>
2022-12-27 18:16:50 +0900

    Update prod.js

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,3 +1,3 @@
 module.exports ={
-    mongoURI: 'mongodb+srv://minhozero:_chlalsgh1@node-react-basic.8s3xciu.mongodb.net/?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://minhozero:_chlalsgh1@node-react-basic.q1nfxbp.mongodb.net/?retryWrites=true&w=majority'
 }
